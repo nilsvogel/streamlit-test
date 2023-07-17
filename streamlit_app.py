98170222bf885e46200f2670db6cdd100791d73a
nilsvogel <nilsvogel94@icloud.com>
2023-07-17 13:59:28 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,35 @@ from streamlit_folium import folium_static
 import pyproj
 import math
 
+def calculate_initial_compass_bearing(pointA, pointB):
+    """
+    Calculates the bearing between two points.
+    The formula used to calculate bearing is:
+        θ = atan2(sin(Δlong).cos(lat2), cos(lat1).sin(lat2) − sin(lat1).cos(lat2).cos(Δlong))
+    :param pointA: tuple of (latitude, longitude)
+    :param pointB: tuple of (latitude, longitude)
+    :returns: initial compass bearing in degrees, as a float
+    """
+
+    if (type(pointA) != tuple) or (type(pointB) != tuple):
+        raise TypeError("Only tuples are supported as arguments")
+
+    lat1 = math.radians(pointA[0])
+    lat2 = math.radians(pointB[0])
+
+    diffLong = math.radians(pointB[1] - pointA[1])
+
+    x = math.sin(diffLong) * math.cos(lat2)
+    y = math.cos(lat1) * math.sin(lat2) - (math.sin(lat1) * math.cos(lat2) * math.cos(diffLong))
+
+    initial_bearing = math.atan2(x, y)
+
+    # Now we have the initial bearing but math.atan2() returns values from -π to + π so we need to normalize the result
+    initial_bearing = math.degrees(initial_bearing)
+    compass_bearing = (initial_bearing + 360) % 360
+
+    return compass_bearing
+
 def get_geodesic_point(start, bearing, distance):
     geod = pyproj.Geod(ellps='WGS84')
     lon, lat, _ = geod.fwd(start[1], start[0], bearing, distance)
